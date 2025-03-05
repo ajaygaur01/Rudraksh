7fc25270c77675ae0e80a0b50fa72f7e393d73fc
Ajay Gaur <gaurajay787@gmail.com>
2025-03-05 21:20:04 +0530

    Products getall and getby ID done

--- src/app/api/products/getall/route.ts
+++ src/app/api/products/getall/route.ts
@@ -1,4 +1,3 @@
-// app/api/products/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
@@ -7,8 +6,10 @@ const prisma = new PrismaClient();
 export async function GET(request: NextRequest) {
   try {
     const products = await prisma.productDetails.findMany();
-    return NextResponse.json(products);
+    return NextResponse.json(products, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: 'Failed to fetch products' }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
   }
 }

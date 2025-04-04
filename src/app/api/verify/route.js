33fabe3cd127ece4a5340153b5ce781a7a9fefcf
Ajay Gaur <gaurajay787@gmail.com>
2025-04-04 23:52:38 +0530

    coo

--- src/app/api/verify/route.js
+++ src/app/api/verify/route.js
@@ -1,9 +1,7 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import { Cashfree } from "cashfree-pg";
-import dotenv from "dotenv";
 
-dotenv.config();
 const prisma = new PrismaClient();
 
 export async function POST(req) {
